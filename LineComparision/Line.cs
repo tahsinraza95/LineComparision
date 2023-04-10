fa51b70ba14b429ad8b002ba4d3dc79c2b5f6f4c
tahsinraza95 <tahsinr693@gmail.com>
2023-04-10 23:49:10 +0530

    [TahsinRaza] Adding Line Comparision

---
+++ LineComparision/Line.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    public class Line
+    {
+        int PointX1, pointY1, poIntX2, pointY2;
+        public Line(int pointX1, int pointY1, int pointX2, int pointY2)
+        {
+            this.PointX1 = pointX1;
+            pointY1 = pointY1;
+            poIntX2 = pointX2;
+            this.pointY2 = pointY2;
+        }
+
+        public double Length()
+        {
+            double length = Math.Sqrt(Math.Pow((poIntX2 - PointX1), 2) + Math.Pow((pointY2 - pointY1), 2));
+            return length;
+        }
+    }
+}
--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -8,9 +8,29 @@ namespace LineComparision
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Line Comparison Program");
+
+            Line line = new Line(1, 2, 3, 4);
+            double length1 = line.Length();
+            Console.WriteLine("Length of 1st Line : " + length1);
+
+            Line line2 = new Line(5, 6, 7, 9);
+            double length2 = line2.Length();
+            Console.WriteLine("Length of 2nd Line : " + length2);
+
+            if (length1.Equals(length2))
+            {
+                Console.WriteLine("Both Line Are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Both Line Are not Equal");
+            }
+            Console.ReadLine();
+
         }
     }
 }
